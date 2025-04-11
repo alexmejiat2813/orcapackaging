3f09015342d697e27212bfe34303333e6059ad1c
alexmejiat2813 <alexmejiat@hotmail.com>
2025-04-10 21:42:52 -0400

    migrations

---
+++ resources/views/suppliesRequest/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Supply Request')
+
+@section('content')
+    <div class="container">
+        <h1>Supply Request</h1>
+        <div id="jqxgrid"></div>
+    </div>
+@endsection
